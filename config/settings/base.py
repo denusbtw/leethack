296e81e5a2a598d2b27d6aac69c65bb45aae0bb7
denusbtw <denisrozum@gmail.com>
2025-05-30 21:53:03 +0300

    feat: create `hackathons` app

--- config/settings/base.py
+++ config/settings/base.py
@@ -48,7 +48,8 @@ THIRD_PARTY_APPS = []
 
 LOCAL_APPS = [
     "leethack.core",
-    "leethack.users"
+    "leethack.users",
+    "leethack.hackathons",
 ]
 
 INSTALLED_APPS = DJANGO_APPS + THIRD_PARTY_APPS + LOCAL_APPS
