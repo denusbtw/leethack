42501ce00f9ce92914b00671a1d6382f6d3fedc9
denusbtw <denisrozum@gmail.com>
2025-05-30 23:15:53 +0300

    feat: create `participations` app

--- config/settings/base.py
+++ config/settings/base.py
@@ -50,6 +50,7 @@ LOCAL_APPS = [
     "leethack.core",
     "leethack.users",
     "leethack.hackathons",
+    "leethack.participations"
 ]
 
 INSTALLED_APPS = DJANGO_APPS + THIRD_PARTY_APPS + LOCAL_APPS
