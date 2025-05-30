59290c4bd6749985e0dc26ca90f663c1279b486d
denusbtw <denisrozum@gmail.com>
2025-05-30 23:17:41 +0300

    feat(hackathons): create Category and Hackathon models + migrations file

--- leethack/hackathons/models.py
+++ leethack/hackathons/models.py
@@ -1,3 +1,62 @@
+from django.conf import settings
 from django.db import models
+from django.utils.text import slugify
+from django.utils.translation import gettext_lazy as _
 
-# Create your models here.
+from leethack.core.models import UUIDModel, TimestampedModel
+
+
+class Category(UUIDModel, TimestampedModel):
+    title = models.CharField(
+        max_length=255,
+        unique=True,
+        help_text=_("Title of category.")
+    )
+    slug = models.SlugField(max_length=255, unique=True, blank=True)
+
+    class Meta:
+        verbose_name_plural = _("Categories")
+
+    def __str__(self):
+        return self.title
+
+    def save(self, *args, **kwargs):
+        self.slug = slugify(self.title)
+        super().save(*args, **kwargs)
+
+
+class Hackathon(UUIDModel, TimestampedModel):
+    host = models.ForeignKey(
+        settings.AUTH_USER_MODEL,
+        on_delete=models.CASCADE,
+        related_name="hosted_hackathons",
+        help_text=_("Organizer of hackathon.")
+    )
+    title = models.CharField(max_length=255, help_text=_("Title of hackathon."))
+    description = models.TextField(help_text=_("Description of hackathon."))
+    category = models.ForeignKey(
+        "Category",
+        on_delete=models.SET_NULL,
+        null=True,
+        help_text=_("Category of hackathon.")
+    )
+    prize = models.PositiveIntegerField(help_text=_("Prize for winning hackathon."))
+    start_datetime = models.DateTimeField(help_text=_("Start date and time of hackathon."))
+    end_datetime = models.DateTimeField(help_text=_("End date and time of hackathon."))
+
+    class Meta:
+        constraints = [
+            models.CheckConstraint(
+                condition=models.Q(start_datetime__lt=models.F("end_datetime")),
+                name="end_datetime_after_start_datetime"
+            )
+        ]
+
+    def __str__(self):
+        return self.title
+
+    @property
+    def is_active(self):
+        from django.utils import timezone
+        now = timezone.now()
+        return self.start_datetime <= now <= self.end_datetime
