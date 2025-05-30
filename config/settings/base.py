171937cd393161cef661a82f48bd27e43a6eec0b
denusbtw <denisrozum@gmail.com>
2025-05-30 23:25:39 +0300

    install djangorestframework

--- config/settings/base.py
+++ config/settings/base.py
@@ -44,7 +44,9 @@ DJANGO_APPS = [
     'django.contrib.staticfiles',
 ]
 
-THIRD_PARTY_APPS = []
+THIRD_PARTY_APPS = [
+    "rest_framework",
+]
 
 LOCAL_APPS = [
     "leethack.core",
@@ -102,3 +104,8 @@ TEMPLATES = [
 
 
 STATIC_URL = 'static/'
+
+
+REST_FRAMEWORK = {
+    "EXCEPTION_HANDLER": "leethack.api.exceptions.custom_exception_handler"
+}
