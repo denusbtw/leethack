6ccf1f4291717817d9f110c5e44c3a23ed97348a
denusbtw <denisrozum@gmail.com>
2025-05-30 21:41:51 +0300

    feat: create  app with abstract models TimestampedModel and UUIDModel

---
+++ leethack/core/models.py
@@ -0,0 +1,19 @@
+import uuid
+
+from django.db import models
+from django.utils import timezone
+
+
+class UUIDModel(models.Model):
+    id = models.UUIDField(primary_key=True, default=uuid.uuid4, editable=False)
+
+    class Meta:
+        abstract = True
+
+
+class TimestampedModel(models.Model):
+    created_at = models.DateTimeField(default=timezone.now, blank=True)
+    updated_at = models.DateTimeField(default=timezone.now, blank=True)
+
+    class Meta:
+        abstract = True
