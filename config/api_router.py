145021812312840db9fc52a908eeb5697e7bd28d
denusbtw <denisrozum@gmail.com>
2025-06-03 19:39:46 +0300

    refactor: move auth endpoints to root urls.py

--- config/api_router.py
+++ config/api_router.py
@@ -2,6 +2,5 @@ from django.urls import path, include
 
 app_name = "api"
 urlpatterns = [
-    path("auth/", include("rest_framework.urls")),
     path("v1/", include("leethack.api.urls.v1")),
 ]
--- config/urls.py
+++ config/urls.py
@@ -7,6 +7,7 @@ from debug_toolbar.toolbar import debug_toolbar_urls
 
 urlpatterns = [
     path("admin/", admin.site.urls),
+    path("api/auth/", include("rest_framework.urls")),
     path("api/", include("config.api_router")),
 ]
 
